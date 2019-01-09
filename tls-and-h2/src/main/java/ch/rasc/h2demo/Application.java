a4663783deab8b7e1077762fa51d93ba5bbe1a84
Ralph Schaer <ralphschaer@gmail.com>
2019-01-09 09:30:34 +0100

    Add tls, http/2 spring boot example

---
+++ tls-and-h2/src/main/java/ch/rasc/h2demo/Application.java
@@ -0,0 +1,38 @@
+package ch.rasc.h2demo;
+
+import javax.servlet.http.PushBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@ResponseBody
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/withoutPush")
+	public String withoutPush() {
+		return "index";
+	}
+	
+	@GetMapping("/withPush")
+	public String withPush(PushBuilder pushBuilder) {
+		if (pushBuilder != null) {
+		    pushBuilder.path("cat.webp").push();
+		}
+		return "index";
+	}
+
+}
