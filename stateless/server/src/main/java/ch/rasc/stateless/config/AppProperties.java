750b263dd223f138fe99d409c7d503e8c5c317ad
Ralph Schaer <ralphschaer@gmail.com>
2019-05-16 10:30:16 +0200

    Fix

--- stateless/server/src/main/java/ch/rasc/stateless/config/AppProperties.java
+++ stateless/server/src/main/java/ch/rasc/stateless/config/AppProperties.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "app")
-@Component
 public class AppProperties {
   private boolean secureCookie;
 
