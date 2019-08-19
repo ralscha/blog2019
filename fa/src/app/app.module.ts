31fd9016d06b976596107de3d7eb9b819418be22
Ralph Schaer <ralphschaer@gmail.com>
2019-08-19 18:28:34 +0200

    Update

--- fa/src/app/app.module.ts
+++ fa/src/app/app.module.ts
@@ -5,19 +5,35 @@ import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {AppComponent} from './app.component';
 import {HomePage} from './home/home.page';
 import {CommonModule} from '@angular/common';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {library} from '@fortawesome/fontawesome-svg-core';
 import {
-  faSmileWink as faSmileWinkSolid, faSmileBeam as faSmileBeanSolid,
-  faAsterisk, faCog, faSkating, faSkiing, faSkiingNordic, faSnowboarding, faSwimmer,
-  faCloud, faMobile, faSpinner, faHandPointLeft,
-  faSync, faPlay, faSun, faMoon, faStar,
-  faEnvelopeOpen, faSquare, faCircle,
+  faAsterisk,
+  faBell as faSolidBell,
+  faCircle,
+  faCloud,
+  faCog,
+  faEnvelopeOpen,
+  faHandPointLeft,
+  faMobile,
+  faMoon,
+  faPlay,
+  faSkating,
+  faSkiing,
+  faSkiingNordic,
+  faSmileBeam as faSmileBeanSolid,
+  faSmileWink as faSmileWinkSolid,
+  faSnowboarding,
+  faSpinner,
+  faSquare,
+  faStar,
+  faSun,
+  faSwimmer,
+  faSync
 } from '@fortawesome/free-solid-svg-icons';
-import {faSmileWink, faSmileBeam} from '@fortawesome/free-regular-svg-icons';
-
+import {faBell as faRegularBell, faSmileBeam, faSmileWink} from '@fortawesome/free-regular-svg-icons';
+// import {fas} from '@fortawesome/free-solid-svg-icons';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -40,18 +56,14 @@ const routes: Routes = [
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor() {
-    library.add(faSmileWinkSolid, faSmileBeanSolid,
-      faSmileWink, faSmileBeam,
-      faEnvelopeOpen,
-      faCloud,
-      faMobile,
-      faSquare,
-      faSpinner,
-      faCircle,
-      faSync,
-      faPlay, faSun, faMoon, faStar,
-      faHandPointLeft,
-      faAsterisk, faCog, faSkating, faSkiing, faSkiingNordic, faSnowboarding, faSwimmer);
+  constructor(library: FaIconLibrary) {
+    // library.addIconPacks(fas);
+    library.addIcons(faSmileWinkSolid, faSmileBeanSolid,
+      faSmileWink, faSmileBeam, faEnvelopeOpen, faCloud,
+      faMobile, faSquare, faSpinner, faCircle,
+      faSync, faPlay, faSun, faMoon, faStar,
+      faHandPointLeft, faAsterisk, faCog, faSkating, 
+      faSkiing, faSkiingNordic, faSnowboarding, faSwimmer,
+      faSolidBell, faRegularBell);
   }
 }
--- fa/src/app/home/home.page.ts
+++ fa/src/app/home/home.page.ts
@@ -1,15 +1,37 @@
-import {Component} from '@angular/core';
-import { faHandPointLeft, faHandPointRight } from '@fortawesome/free-regular-svg-icons';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {faHandPointLeft, faHandPointRight} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
   faHandPointLeft = faHandPointLeft;
   faHandPointRight = faHandPointRight;
 
   syncRunning = false;
   magicLevel = 0;
+
+  color = 'red';
+  private interval: number;
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  ngOnInit(): void {
+    this.interval = setInterval(() => this.color = this.randomColor(), 1000);
+  }
+
+  dynamicStyle() {
+    return {
+      color: this.randomColor(),
+      stroke: this.randomColor()
+    };
+  }
+
+  private randomColor(): string {
+    return '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
+  }
 }
