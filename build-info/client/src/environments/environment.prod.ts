946bda7d8468cebb500c47851c6187b0551272c9
Ralph Schaer <ralphschaer@gmail.com>
2020-07-01 03:23:10 +0200

    release v0.0.1

--- build-info/client/src/environments/environment.prod.ts
+++ build-info/client/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   serverURL: 'http://localhost:8080',
   version: '0.0.1',
-  buildTimestamp: 1593542780,
-  shortCommitId: '90a40d6',
-  commitId: '90a40d66c9c70001e47264271e592776d9a4c6d4',
-  commitTime: 1593514358
+  buildTimestamp: 1593566538,
+  shortCommitId: 'f61ac1e',
+  commitId: 'f61ac1e319f71cba5ac0af9219c90081d26d5ae6',
+  commitTime: 1593542832
 };
