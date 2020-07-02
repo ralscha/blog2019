f768df1675016826ec9f62c66c02fdab7202b212
Ralph Schaer <ralphschaer@gmail.com>
2020-07-02 08:43:03 +0200

    Upgrade to Angular 10

--- credential/client/src/app/auth.guard.ts
+++ credential/client/src/app/auth.guard.ts
@@ -13,7 +13,8 @@ export class AuthGuard implements CanActivate {
   constructor(private readonly authService: AuthService, private readonly router: Router) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.authService.isLoggedIn()) {
       return true;
     }
