0ac2b3f08b8e45260c2ff4344a35e7c4bffd1d3c
Ralph Schaer <ralphschaer@gmail.com>
2025-01-03 19:55:28 +0100

    Upgrade

--- fa/src/app/home/home.page.ts
+++ fa/src/app/home/home.page.ts
@@ -2,9 +2,10 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {faHandPointLeft, faHandPointRight} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss']
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
+    standalone: false
 })
 export class HomePage implements OnInit, OnDestroy {
   faHandPointLeft = faHandPointLeft;
