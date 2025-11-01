38b8bbf2c6b9b0fac0de3dcbd55656134287a1bc
Ralph Schaer <ralphschaer@gmail.com>
2025-11-01 19:46:09 +0100

    Upgrade to Hibernate 7.2

--- envers/src/main/java/ch/rasc/envers/CustomRevisionEntity.java
+++ envers/src/main/java/ch/rasc/envers/CustomRevisionEntity.java
@@ -1,19 +1,52 @@
 package ch.rasc.envers;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import java.util.Date;
 
-import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
+import org.hibernate.envers.RevisionNumber;
+import org.hibernate.envers.RevisionTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "REVINFO")
 @RevisionEntity(CustomRevisionEntityListener.class)
-public class CustomRevisionEntity extends DefaultRevisionEntity {
+public class CustomRevisionEntity {
 
-  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue
+  @RevisionNumber
+  private int id;
+
+  @RevisionTimestamp
+  private long timestamp;
 
   private String username;
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public long getTimestamp() {
+    return this.timestamp;
+  }
+
+  public void setTimestamp(long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  @Transient
+  public Date getRevisionDate() {
+    return new Date(this.timestamp);
+  }
 
   public String getUsername() {
     return this.username;
