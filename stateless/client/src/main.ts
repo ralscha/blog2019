2f0e76cfc1fbad033be2d03672c930ec9fbe27da
Ralph Schaer <ralphschaer@gmail.com>
2025-12-07 10:56:16 +0100

    upgrade

--- stateless/client/src/main.ts
+++ stateless/client/src/main.ts
@@ -3,7 +3,7 @@ import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone
 import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {HomePage} from './app/home/home.page';
-import {inject} from '@angular/core';
+import {inject, provideZoneChangeDetection} from '@angular/core';
 import {AuthGuard} from './app/service/auth.guard';
 import {LoginPage} from './app/login/login.page';
 import {AppComponent} from './app/app.component';
@@ -32,7 +32,7 @@ const routes: Routes = [
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideIonicAngular(),
+    provideZoneChangeDetection(),provideIonicAngular(),
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes, withHashLocation())
