86eb507d34f6ea1132b6b0196b74a279747aaebf
Ralph Schaer <ralphschaer@gmail.com>
2026-03-19 15:09:54 +0100

    update

--- tls-and-h2/src/main/java/ch/rasc/h2demo/Application.java
+++ tls-and-h2/src/main/java/ch/rasc/h2demo/Application.java
@@ -2,37 +2,20 @@ package ch.rasc.h2demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import jakarta.servlet.http.PushBuilder;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
-  @ResponseBody
   @GetMapping("/")
   public String helloWorld() {
     return "Hello World";
-  }
-
-  @GetMapping("/withoutPush")
-  public String withoutPush() {
-    return "index";
-  }
-
-  @GetMapping("/withPush")
-  public String withPush(PushBuilder pushBuilder) {
-    if (pushBuilder != null) {
-      pushBuilder.path("cat.webp").push();
-    }
-    return "index";
   }
 
 }
